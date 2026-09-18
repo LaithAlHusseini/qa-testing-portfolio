ed27094cdaec1c43ef4d863ff99632f39fea9537
Laith Al-Hussiene <laith.muhannad6@gmail.com>
2026-09-18 23:16:06 +0300

    Centralize Playwright test data

--- automation/playwright/tests/login.spec.ts
+++ automation/playwright/tests/login.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { users } from '../test-data/users';
 
 test.describe('Login Tests', () => {
   let loginPage: LoginPage;
@@ -10,13 +11,19 @@ test.describe('Login Tests', () => {
   });
 
   test('successful login with valid credentials', async () => {
-    await loginPage.login('standard_user', 'secret_sauce');
+   await loginPage.login(
+  users.standard.username,
+  users.standard.password
+);
 
     await loginPage.expectLoginSuccess();
   });
 
   test('login fails with invalid password', async () => {
-    await loginPage.login('standard_user', 'wrong_password');
+    await loginPage.login(
+  users.invalid.username,
+  users.invalid.password
+);
 
     await loginPage.expectError(
       'Username and password do not match'
@@ -24,7 +31,7 @@ test.describe('Login Tests', () => {
   });
 
   test('login fails when username is empty', async () => {
-    await loginPage.login('', 'secret_sauce');
+    await loginPage.login('', users.standard.password);
 
     await loginPage.expectError(
       'Username is required'
@@ -32,7 +39,7 @@ test.describe('Login Tests', () => {
   });
 
   test('login fails when password is empty', async () => {
-    await loginPage.login('standard_user', '');
+    await loginPage.login(users.standard.username, '');
 
     await loginPage.expectError(
       'Password is required'
@@ -40,8 +47,10 @@ test.describe('Login Tests', () => {
   });
 
   test('locked out user cannot login', async () => {
-    await loginPage.login('locked_out_user', 'secret_sauce');
-
+    await loginPage.login(
+  users.locked.username,
+  users.locked.password
+);
     await loginPage.expectError(
       'Sorry, this user has been locked out'
     );
