eb75a1e9a15400413455f1536a0b90d469a1bd5d
Laith Al-Hussiene <laith.muhannad6@gmail.com>
2026-09-19 03:44:54 +0300

    Add reusable checkout setup fixture

--- automation/playwright/fixtures/testFixtures.ts
+++ automation/playwright/fixtures/testFixtures.ts
@@ -1,33 +1,84 @@
-import { test as base, expect } from '@playwright/test';
+import {
+  test as base,
+  expect,
+  type Page
+} from '@playwright/test';
 
 import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/InventoryPage';
 import { CartPage } from '../pages/CartPage';
 import { CheckoutPage } from '../pages/CheckoutPage';
+import { users } from '../test-data/users';
 
 type TestFixtures = {
   loginPage: LoginPage;
+  authenticatedPage: Page;
   inventoryPage: InventoryPage;
   cartPage: CartPage;
   checkoutPage: CheckoutPage;
+  checkoutReadyPage: CheckoutPage;
 };
 
 export const test = base.extend<TestFixtures>({
+
+  // Used by login tests without authentication
   loginPage: async ({ page }, use) => {
     await use(new LoginPage(page));
   },
 
-  inventoryPage: async ({ page }, use) => {
-    await use(new InventoryPage(page));
+  // Automatically logs in standard user
+  authenticatedPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+
+    await loginPage.goto();
+
+    await loginPage.login(
+      users.standard.username,
+      users.standard.password
+    );
+
+    await inventoryPage.expectLoaded();
+
+    await use(page);
   },
 
-  cartPage: async ({ page }, use) => {
-    await use(new CartPage(page));
+  inventoryPage: async ({ authenticatedPage }, use) => {
+    await use(
+      new InventoryPage(authenticatedPage)
+    );
   },
 
-  checkoutPage: async ({ page }, use) => {
-    await use(new CheckoutPage(page));
+  cartPage: async ({ authenticatedPage }, use) => {
+    await use(
+      new CartPage(authenticatedPage)
+    );
   },
+
+  checkoutPage: async ({ authenticatedPage }, use) => {
+    await use(
+      new CheckoutPage(authenticatedPage)
+    );
+  },
+checkoutReadyPage: async ({
+  inventoryPage,
+  cartPage,
+  checkoutPage
+}, use) => {
+  await inventoryPage.addBackpackToCart();
+  await inventoryPage.expectCartCount('1');
+
+  await inventoryPage.openCart();
+
+  await cartPage.expectLoaded();
+  await cartPage.expectBackpackVisible();
+
+  await cartPage.startCheckout();
+
+  await checkoutPage.expectInformationPage();
+
+  await use(checkoutPage);
+},
 });
 
 export { expect };
--- automation/playwright/tests/checkout.spec.ts
+++ automation/playwright/tests/checkout.spec.ts
@@ -1,72 +1,43 @@
 import { test } from '../fixtures/testFixtures';
-import { users } from '../test-data/users';
 import { checkoutData } from '../test-data/checkoutData';
 
 test.describe('Checkout Tests', () => {
 
-  test.beforeEach(async ({
-    loginPage,
-    inventoryPage,
-    cartPage,
-    checkoutPage
-  }) => {
-    await loginPage.goto();
-
-    await loginPage.login(
-      users.standard.username,
-      users.standard.password
-    );
-
-    await inventoryPage.expectLoaded();
-
-    await inventoryPage.addBackpackToCart();
-    await inventoryPage.expectCartCount('1');
-
-    await inventoryPage.openCart();
-
-    await cartPage.expectLoaded();
-    await cartPage.expectBackpackVisible();
-
-    await cartPage.startCheckout();
-
-    await checkoutPage.expectInformationPage();
-  });
-
   test('user can complete checkout successfully', async ({
-    checkoutPage
+    checkoutReadyPage
   }) => {
-    await checkoutPage.fillCustomerInformation(
+    await checkoutReadyPage.fillCustomerInformation(
       checkoutData.validCustomer.firstName,
       checkoutData.validCustomer.lastName,
       checkoutData.validCustomer.postalCode
     );
 
-    await checkoutPage.continueCheckout();
+    await checkoutReadyPage.continueCheckout();
 
-    await checkoutPage.expectOverviewPage();
-    await checkoutPage.expectBackpackVisible();
+    await checkoutReadyPage.expectOverviewPage();
+    await checkoutReadyPage.expectBackpackVisible();
 
-    await checkoutPage.finishCheckout();
+    await checkoutReadyPage.finishCheckout();
 
-    await checkoutPage.expectOrderComplete();
+    await checkoutReadyPage.expectOrderComplete();
   });
 
   test('checkout fails when postal code is missing', async ({
-    checkoutPage
+    checkoutReadyPage
   }) => {
-    await checkoutPage.fillCustomerInformation(
+    await checkoutReadyPage.fillCustomerInformation(
       checkoutData.validCustomer.firstName,
       checkoutData.validCustomer.lastName,
       ''
     );
 
-    await checkoutPage.continueCheckout();
+    await checkoutReadyPage.continueCheckout();
 
-    await checkoutPage.expectError(
+    await checkoutReadyPage.expectError(
       'Postal Code is required'
     );
 
-    await checkoutPage.expectInformationPage();
+    await checkoutReadyPage.expectInformationPage();
   });
 
 });
